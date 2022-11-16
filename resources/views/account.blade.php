2659e5027ca04af0cb9d53a9532672d127ef51c8
Gian Daño <112839733+giaisle@users.noreply.github.com>
2022-11-16 19:30:18 +0800

    account-changes (#13)

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -14,13 +14,13 @@
 	<link rel="stylesheet" type="text/css" href="slide navbar style.css">
 <link href="https://fonts.googleapis.com/css2?family=Jost:wght@500&display=swap" rel="stylesheet">
 </head>
-<body style="background-color: rgb(131, 127, 116)">
+<body style="background-image: url('citizen.jpg')">
 	<div class="main">
 		<input type="checkbox" id="chk" aria-hidden="true">
 			<div class="form">
 				<form>
 					<img src="{{ asset('images/logo.png')}}" alt="" id= "logo" >
-					<label for="chk" aria-hidden="true">Sign Up</label>
+					<label for="chk" aria-hidden="true">Create an Account</label>
 					<p>The system's authorized users are as follows. Please select who you want to gain access from.</p>
 					<button class="btn"><a href="citizen">Citizen</button></a>
 					<button class="btn"><a href="establishment">Establishment</button></a>
