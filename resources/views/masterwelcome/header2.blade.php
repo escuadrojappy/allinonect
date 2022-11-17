580159f4d711f02ba08d0337801ea8dd453ac76d
escuadrojappy <112836763+escuadrojappy@users.noreply.github.com>
2022-11-17 23:49:45 +0800

    Template (#29)
    * template part 1
    
    * template-changes-sa-welcome

---
+++ resources/views/masterwelcome/header2.blade.php
@@ -0,0 +1,5 @@
+<meta charset="utf-8">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
+<link href="https://fonts.googleapis.com/css2?family=Jost:wght@500&display=swap" rel="stylesheet">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>{{$title}}</title>
