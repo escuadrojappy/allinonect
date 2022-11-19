976fc2ea50202418d5aa9eed91f9b396197301e4
Gian Daño <112839733+giaisle@users.noreply.github.com>
2022-11-20 04:25:57 +0800

    contactpage-changes (#35)
    contactpage-changes

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,44 +1,76 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
+
+<head>
     <title>Contact Us</title>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="{{ asset('css/contact.css') }}">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   </head>
-<body>
-  <div class="container">
-    <div class="post">
-      <div class="qwe">Thankyou for reaching out to us!
-      </div>
-    </div>
-    <div class="content">
-      <div class="left-side">
-        <div class="address details">
-          <i class="fas fa-map-marker-alt"></i>
-          <div class="topic">Address</div>
-          <div class="text-one">Cabanatuan City</div>
-          <div class="text-two">Nueva Ecija</div>
-        </div>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
 
-        <div class="phone details">
-          <i class="fas fa-phone-alt"></i>
-          <div class="topic">Phone</div>
-          <div class="text-one">+63 9876543210</div>
-          <div class="text-two">+63 1234567890</div>
+<body style=" background: #113153;">
+
+  
+    <div class="container">
+        <div class="post">
+            <div class="qwe">Thank you for contacting us!
+            </div>
         </div>
-        <div class="email details">
-          <i class="fas fa-envelope"></i>
-          <div class="topic">Email</div>
-          <div class="text-one">pedro@gmail.com</div>
-          <div class="text-two">allinonect@yahoo.com</div>
-        </div>
-      </div>
-      <div class="right-side">
-        <div class="topic-text">Send us a message</div>
-        <p>If you have questions, we will answer it! Feel free to contact us through our phone number and email.</p>
-      <form action="#">
+        <div class="content">
+            <div class="left-side">
+                <div class="address details">
+                    <i class="fas fa-map-marker-alt"></i>
+                    <div class="topic">Address</div>
+                    <div class="text-one">Cabanatuan City</div>
+                    <div class="text-two">Nueva Ecija</div>
+                </div>
+
+                <div class="phone details">
+                    <i class="fas fa-phone-alt"></i>
+                    <div class="topic">Phone</div>
+                    <div class="text-one">+63 9876543210</div>
+                    <div class="text-two">+63 1234567890</div>
+                </div>
+                <div class="email details">
+                    <i class="fas fa-envelope"></i>
+                    <div class="topic">Email</div>
+                    <div class="text-one">pedro@gmail.com</div>
+                    <div class="text-two">allinonect@yahoo.com</div>
+                </div>
+              
+            </div>
+            <div class="right-side">
+            <div class="icon"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-send-plus" viewBox="0 0 16 16">
+              <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855a.75.75 0 0 0-.124 1.329l4.995 3.178 1.531 2.406a.5.5 0 0 0 .844-.536L6.637 10.07l7.494-7.494-1.895 4.738a.5.5 0 1 0 .928.372l2.8-7Zm-2.54 1.183L5.93 9.363 1.591 6.602l11.833-4.733Z"/>
+              <path d="M16 12.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Zm-3.5-2a.5.5 0 0 0-.5.5v1h-1a.5.5 0 0 0 0 1h1v1a.5.5 0 0 0 1 0v-1h1a.5.5 0 0 0 0-1h-1v-1a.5.5 0 0 0-.5-.5Z"/>
+            </svg></div>
+                <div class="topic-text">Make contact with us</div>
+                <p>Tell us what you think, and we'll get back to you as soon as possible; please contact us using the details provided on this page.
+                  <br><br>For more information, please visit our social media accounts.</p>
+                <div class="input-group flex-nowrap">
+                  <span class="input-group-text" id="addon-wrapping"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+                    <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
+                  </svg></span>
+                  <input type="text" disabled class="form-control" placeholder="www.allinonect.com" aria-label="Username">
+                </div>
+                <div class="input-group flex-nowrap">
+                  <span class="input-group-text" id="addon-wrapping"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16">
+                    <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/>
+                  </svg></span>
+                  <input type="text" disabled class="form-control" placeholder="www.allinonect.com" aria-label="Username">
+                </div>
+                <div class="input-group flex-nowrap">
+                  <span class="input-group-text" id="addon-wrapping"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-camera-fill" viewBox="0 0 16 16">
+                    <path d="M10.5 8.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/>
+                    <path d="M2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4H2zm.5 2a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1zm9 2.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0z"/>
+                  </svg></span>
+                  <input type="text" disabled class="form-control" placeholder="www.allinonect.com" aria-label="Username">
+                </div>
+                <button class="btn btn-primary">Back to Home</button>
+
+                {{-- <form action="#">
         <div class="input-box">
           <input type="text" placeholder="Enter your name">
         </div>
@@ -49,24 +81,28 @@
           <textarea placeholder="Enter your Message"></textarea>
         </div>
         <div class="btn">
-          <button type="submit">Send</button>
+          <button class="submit" type="submit">Send</button>
 
         </div>
 
-      </form>
+      </form> --}}
+                
+            </div>
+        </div>
     </div>
-    </div>
-  </div>
-  <script>
-    const btn = document.querySelector("button");
-    const post = document.querySelector(".post");
-    const content = document.querySelector(".content")
-    btn.onclick = ()=>{
-    content.style.display = "none";
-    post.style.display = " block";
-    }
-  </script>
+  
+
+    <script>
+        const btn = document.querySelector(".submit");
+        const post = document.querySelector(".post");
+        const content = document.querySelector(".content")
+        btn.onclick = () => {
+            content.style.display = "none";
+            post.style.display = " block";
+        }
+    </script>
 
 
 </body>
+
 </html>
