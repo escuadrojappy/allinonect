2a2f9f3885bf57c571284e8a4de09440913290d7
Marina Suzanne <112848414+marina-suzanne@users.noreply.github.com>
2022-11-21 22:49:20 +0800

    Establishment Frontend pt.2 (#41)
    * Establishment Master Template Front End pt.1
    
    * Establishment Frontend pt.2

--- resources/views/establishment_master_template/sidebar.blade.php
+++ resources/views/establishment_master_template/sidebar.blade.php
@@ -12,6 +12,14 @@
                     <i class="material-icons" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">keyboard_arrow_down</i>
                     <ul class="dropdown-menu pull-right">
                         <li>
+                            <a href="/allinonect/public/establishment/setting">
+                                <i class="material-icons">
+                                    settings
+                                </i>
+                                Setting
+                            </a>
+                        </li>
+                        <li>
                             <a href="/allinonect/public/login/establishment">
                                 <i class="material-icons">
                                     input
@@ -34,64 +42,6 @@
                         <span>Dashboard</span>
                     </a>
                 </li>
-                {{-- <li>
-                    <a href="">
-                        <i class="material-icons">text_fields</i>
-                        <span></span>
-                    </a>
-                </li>
-                <li>
-                    <a href="pages/helper-classes.html">
-                        <i class="material-icons">layers</i>
-                        <span>Helper Classes</span>
-                    </a>
-                </li> --}}
-                {{-- <li>
-                    <a href="javascript:void(0);" class="menu-toggle">
-                        <i class="material-icons">widgets</i>
-                        <span>Widgets</span>
-                    </a>
-                    <ul class="ml-menu">
-                        <li>
-                            <a href="javascript:void(0);" class="menu-toggle">
-                                <span>Cards</span>
-                            </a>
-                            <ul class="ml-menu">
-                                <li>
-                                    <a href="pages/widgets/cards/basic.html">Basic</a>
-                                </li>
-                                <li>
-                                    <a href="pages/widgets/cards/colored.html">Colored</a>
-                                </li>
-                                <li>
-                                    <a href="pages/widgets/cards/no-header.html">No Header</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a href="javascript:void(0);" class="menu-toggle">
-                                <span>Infobox</span>
-                            </a>
-                            <ul class="ml-menu">
-                                <li>
-                                    <a href="pages/widgets/infobox/infobox-1.html">Infobox-1</a>
-                                </li>
-                                <li>
-                                    <a href="pages/widgets/infobox/infobox-2.html">Infobox-2</a>
-                                </li>
-                                <li>
-                                    <a href="pages/widgets/infobox/infobox-3.html">Infobox-3</a>
-                                </li>
-                                <li>
-                                    <a href="pages/widgets/infobox/infobox-4.html">Infobox-4</a>
-                                </li>
-                                <li>
-                                    <a href="pages/widgets/infobox/infobox-5.html">Infobox-5</a>
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
-                </li> --}}
                 <li>
                     <a href="javascript:void(0);" class="menu-toggle">
                         <i class="material-icons">people</i>
@@ -105,13 +55,6 @@
                             <a href="/allinonect/public/establishment/contacts/contacttracing">Contact Tracing</a>
                         </li>
                     </ul>
-                </li>
-
-                <li class="{{ Request::is('establishment/setting') ? 'active' : '' }}">
-                    <a href="/allinonect/public/establishment/setting">
-                        <i class="material-icons">settings</i>
-                        <span>Settings</span>
-                    </a>
                 </li>
                 
             </ul>
