a885db2ffb5dcca527254fc737ee2f54b3508d55
escuadrojappy <112836763+escuadrojappy@users.noreply.github.com>
2022-11-25 18:28:22 +0800

    contact-report

--- resources/views/admin_master_template/navbar.blade.php
+++ resources/views/admin_master_template/navbar.blade.php
@@ -1,9 +1,9 @@
 <nav class="navbar">
-    <div class="container-fluid">
+    <div class="container-fluid topcolor">
         <div class="navbar-header">
             <a href="javascript:void(0);" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false"></a>
             <a href="javascript:void(0);" class="bars"></a>
-            <a class="navbar-brand" href="index.html">DEPARTMENT OF HEALTH - ADMINISTRATOR</a>
+            <a class="navbar-brand" >DEPARTMENT OF HEALTH - ADMINISTRATOR</a>
         </div>
     </div>
 </nav>
