833cadf078a3a8e831af49bc956fe9e4f4ab61f1
escuadrojappy <112836763+escuadrojappy@users.noreply.github.com>
2022-12-16 02:33:18 +0800

    Update common.js

--- public/js/common.js
+++ public/js/common.js
@@ -8,7 +8,6 @@ var apiUrl = 'http://localhost/allinonect/public/api/'
 var webUrl = 'http://localhost/allinonect/public/'
 
 if (window.location.origin.indexOf('aioctracing') > -1) {
-    alert('qwe')
     var apiUrl = 'https://aioctracing.com/api/'
     var webUrl = 'http://aioctracing.com/'
 }
