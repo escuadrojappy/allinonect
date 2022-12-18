fc1ecb4c37aed46bb83feb276cbcb899556a51e7
Marina Suzanne Cancio <cancio.marinasuzanne@gmail.com>
2022-12-18 17:21:44 +0800

    Update EstablishmentService.php

--- app/Api/Services/EstablishmentService.php
+++ app/Api/Services/EstablishmentService.php
@@ -224,10 +224,10 @@ class EstablishmentService extends Service
 
         $xlsxName = str_replace(' ', '-', Arr::get(auth()->user(), 'establishment.name')). '-contact-report-'. date('Y-m-d-H-i-s'). '.xlsx';
 
-        Excel::store(new EstablishmentContactTracingExport($result), sprintf('%s/%s', 'contact-tracing', $xlsxName));
+        // Excel::store(new EstablishmentContactTracingExport($result), sprintf('%s/%s', 'contact-tracing', $xlsxName));
 
-        $filePath = sprintf('%s\%s\%s', config('filesystems.disks.local.root'), 'contact-tracing', $xlsxName);
-        dd($filePath);
+        $filePath = sprintf('%s\%s\%s', config('filesystems.disks.local.root'), 'contact-tracing', 'Hello-contact-report-2022-12-18-17-18-26.xlsx');
+        // dd($filePath);
         dd(response()->download($filePath, $xlsxName, [
             'Content-type' => 'application/vnd.ms-excel',
             'filename' => $xlsxName
