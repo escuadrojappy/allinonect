d7b436fd385af7766f8432c7bae071af63c3eeb7
Marina Suzanne Cancio <cancio.marinasuzanne@gmail.com>
2022-12-18 17:15:33 +0800

    Update EstablishmentService.php

--- app/Api/Services/EstablishmentService.php
+++ app/Api/Services/EstablishmentService.php
@@ -220,11 +220,11 @@ class EstablishmentService extends Service
      */
     public function generateContactTracingReport(array $request)
     {
-        dd('hello');
         $result = $this->establishmentContactTracingRepository->search($request);
+
         $xlsxName = str_replace(' ', '-', Arr::get(auth()->user(), 'establishment.name')). '-contact-report-'. date('Y-m-d-H-i-s'). '.xlsx';
-        dd(sprintf('%s/%s', 'contact-tracing', $xlsxName), $xlsxName);
-        dd(Excel::store(new EstablishmentContactTracingExport($result), sprintf('%s/%s', 'contact-tracing', $xlsxName)));
+
+        Excel::store(new EstablishmentContactTracingExport($result), sprintf('%s/%s', 'contact-tracing', $xlsxName));
 
         $filePath = sprintf('%s\%s\%s', config('filesystems.disks.local.root'), 'contact-tracing', $xlsxName);
 
