4ddb42ff50df03b89e939614c38f2d14851bdb99
escuadrojappy <112836763+escuadrojappy@users.noreply.github.com>
2023-01-13 01:30:49 +0800

    Cleaning of frontend (#113)
    * cleaning part 1
    
    * pt 2
    
    * pt 3
    
    * malapit na sa katapusan

--- resources/views/establishment_contents/my_profile.blade.php
+++ resources/views/establishment_contents/my_profile.blade.php
@@ -37,7 +37,7 @@
             </div>
         </div>
     </div>
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+    <!-- <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
         <div class="card">
             <div class="header bg-indigo">
                 <h2>
@@ -70,7 +70,7 @@
                 </form>
             </div>
         </div>
-    </div>
+    </div> -->
 
 
 
