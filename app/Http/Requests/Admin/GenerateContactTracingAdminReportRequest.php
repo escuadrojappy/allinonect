62770ca114579f28605882c96a8a55073032bf68
escuadrojappy <112836763+escuadrojappy@users.noreply.github.com>
2023-01-13 01:19:03 +0800

    Update GenerateContactTracingAdminReportRequest.php

--- app/Http/Requests/Admin/GenerateContactTracingAdminReportRequest.php
+++ app/Http/Requests/Admin/GenerateContactTracingAdminReportRequest.php
@@ -55,7 +55,12 @@ class GenerateContactTracingAdminReportRequest extends FormRequest
                 'date',
                 'after:date_range.*.start_date'
             ],
-
+            'establishment_id' => [
+                'required',
+                'numeric',
+                'exists:App\Models\Establishment,id',
+            ],
+            
         ];
     }
 }
