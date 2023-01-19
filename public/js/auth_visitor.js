b0eb128c7e3565a8a56ac9003f3f8dd695a06cbe
escuadrojappy <112836763+escuadrojappy@users.noreply.github.com>
2023-01-19 16:29:33 +0800

    Update auth_visitor.js

--- public/js/auth_visitor.js
+++ public/js/auth_visitor.js
@@ -3,6 +3,17 @@ function authVisitor () {
         if (result.role_id !== 3) {
             return get(`${apiUrl}auth/logout`)
         } else {
+            if (result.visitor.health_status === null) {
+                var covidResult = 'NEGATIVE'
+            } else {
+                if (result.visitor.health_status.covid_result === 1){
+
+                var covidResult = 'POSITIVE'
+                } 
+                else{
+                    covidResult = 'NEGATIVE'
+                }  
+            }
             authCommon = result
             $('.page-loader-wrapper').fadeOut()
             $('.name-visitor').text(result.visitor.first_name + " " + result.visitor.last_name)
@@ -10,7 +21,6 @@ function authVisitor () {
             $('.address-visitor').text(result.visitor.place_of_birth)
             $('.birthdate-visitor').text(result.visitor.birthdate)
             $('.contact-number-visitor').text(result.visitor.contact_number)
-            $('.covid-result-visitor').text(result.visitor.health_status.covid_result ? 'POSTIVE' : 'NEGATIVE')
             $('.covid-result-visitor').text(covidResult)
         }
     }).then((result) => {
